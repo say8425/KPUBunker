8e290cac0a40cb030bacbb1728097e76225e961d
northPenguin <say8425@gmail.com>
2015-04-19 21:47:21 +0900

    Devise is initialized

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   root 'lectures#index'
 
   resources :lectures
+  
+  devise_for :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
