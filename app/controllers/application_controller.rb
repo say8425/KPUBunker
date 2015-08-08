96119476cc74a6d9e81498721f29866dd6e70b13
Penguin <say8425@gmail.com>
2015-08-08 21:54:34 +0900

    authenticate_admin! has been added. Also current_admin added cuz comfortable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # before_action :authenticate_user!
+
+  def authenticate_admin!
+    unless current_admin?
+      head 403
+      false
+    end
+  end
+
+  def current_admin?
+    current_user.try(:admin?)
+  end
 end
