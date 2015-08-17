105b6c92109e5ff50b8319d2899f6209157491a7
Penguin <say8425@gmail.com>
2015-08-18 00:19:43 +0900

    The integer point will be show with grade

--- app/helpers/lectures_helper.rb
+++ app/helpers/lectures_helper.rb
@@ -1,2 +1,19 @@
 module LecturesHelper
+  def makeGradeString(lecture)
+    grade = lecture.grade
+    case
+      when grade <= 1.0
+        'F'
+      when grade <= 2.0
+        'E'
+      when grade <= 3.0
+        'D'
+      when grade <= 4.0
+        'C'
+      when grade <= 5.0
+        'B'
+      when grade <= 6.0
+        'A'
+    end
+  end
 end
